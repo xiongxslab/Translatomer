c19a21e71f6391207d629e284f7992863bd5357d
xiongxslab <116224972+xiongxslab@users.noreply.github.com>
2023-11-13 22:19:32 +0800

    Create blocks.py

---
+++ model/blocks.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import copy
+
+class TransformerLayer(torch.nn.TransformerEncoderLayer):
+    def forward(self, src, src_mask = None, src_key_padding_mask = None):
+        # MHA section
+        src_norm = self.norm1(src)
+        src_side, attn_weights = self.self_attn(src_norm, src_norm, src_norm, 
+                                    attn_mask=src_mask,
+                                    key_padding_mask=src_key_padding_mask)
+        src = src + self.dropout1(src_side)
+
+        # MLP section
+        src_norm = self.norm2(src)
+        src_side = self.linear2(self.dropout(self.activation(self.linear1(src_norm))))
+        src = src + self.dropout2(src_side)
+        return src, attn_weights
+
+class TransformerEncoder(torch.nn.TransformerEncoder):
+
+    def __init__(self, encoder_layer, num_layers, norm=None, record_attn = False):
+        super(TransformerEncoder, self).__init__(encoder_layer, num_layers)
+        self.layers = self._get_clones(encoder_layer, num_layers)
+        self.num_layers = num_layers
+        self.norm = norm
+        self.record_attn = record_attn
+
+    def forward(self, src, mask = None, src_key_padding_mask = None):
+        r"""Pass the input through the encoder layers in turn.
+
+        Args:
+            src: the sequence to the encoder (required).
+            mask: the mask for the src sequence (optional).
+            src_key_padding_mask: the mask for the src keys per batch (optional).
+        """
+        output = src
+
+        attn_weight_list = []
+
+        for mod in self.layers:
+            output, attn_weights = mod(output, src_mask=mask, src_key_padding_mask=src_key_padding_mask)
+            attn_weight_list.append(attn_weights.unsqueeze(0).detach())
+        if self.norm is not None:
+            output = self.norm(output)
+
+        if self.record_attn:
+            return output, torch.cat(attn_weight_list)
+        else:
+            return output
+
+    def _get_clones(self, module, N):
+        return torch.nn.modules.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, hidden, dropout = 0.1, max_len = 512):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, hidden, 2) * (-np.log(10000.0) / hidden))
+        pe = torch.zeros(max_len, 1, hidden)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+class AttnModule(nn.Module):
+    def __init__(self, hidden = 512, layers = 12, record_attn = False, inpu_dim = 512):
+        super(AttnModule, self).__init__()
+
+        self.record_attn = record_attn
+        self.pos_encoder = PositionalEncoding(hidden, dropout = 0.1)
+        encoder_layers = TransformerLayer(hidden, 
+                                          nhead = 8,
+                                          dropout = 0.1,
+                                          dim_feedforward = 512,
+                                          batch_first = True)
+        self.module = TransformerEncoder(encoder_layers, 
+                                         layers, 
+                                         record_attn = record_attn)
+
+    def forward(self, x):
+        x = self.pos_encoder(x)
+        output = self.module(x)
+        return output
+
+    def inference(self, x):
+        return self.module(x)
+
+if __name__ == '__main__':
+    main()
